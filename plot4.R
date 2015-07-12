731ceef6a34b2656076f09b85f5263f42aa6940b
emilywhsu <emilywhsu@yahoo.com>
2015-07-12 04:29:49 -0700

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,18 @@
+loading<-read.table("/Users/emilyhsu/CourseEra/household_power_consumption.txt",header=T,sep=";",colClasses=c("character","character","double","double","double","double","double","double","numeric"),na.strings="?")
+loading$DateTime=paste(loading$Date, loading$Time)
+loading$DateTime=as.POSIXlt(loading$DateTime,format="%d/%m/%Y %H:%M:%S")
+loading$Date=NULL
+loading$Time=NULL
+subloading<-subset(loading,DateTime$year==107 & DateTime$mon==1 & (DateTime$mday==1 | DateTime$mday==2))
+graph<-par()
+png(filename="plot4.png")
+par(mfrow=c(2,2))
+plot(x=(subloading$DateTime),y=subloading$Global_active_power,type="l",xlab="",ylab="Global Active Power (kilowatts)")
+plot(x=(subloading$DateTime),y=subloading$Voltage,type="l",xlab="datetime",ylab="Voltage")
+plot(x=(subloading$DateTime),y=subloading$Sub_metering_1,type="l",xlab="",ylab="Energy sub metering")
+lines(x=(subloading$DateTime),y=subloading$Sub_metering_2,col="red")
+lines(x=(subloading$DateTime),y=subloading$Sub_metering_3,col="blue")
+legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),bty="l",col=c("black","red","blue"),lwd=2)
+plot(subloading$DateTime,subloading$Global_reactive_power,type="l",main="",xlab="datetime")
+par(graph)
+dev.off()
